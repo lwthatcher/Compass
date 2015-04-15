0554e572830c66686a8d6f5ae0c0302d6c15ad4a
Lawrence Thatcher <lwthatcher@msn.com>
2015-04-15 17:30:43 -0600

    updated vax game (still buggy)
    In the quarantine phase it keeps on selecting the same invalid node to
    click.

--- test/d3/demo/NodeSizeComparator.java
+++ test/d3/demo/NodeSizeComparator.java
@@ -18,10 +18,10 @@ public class NodeSizeComparator implements Comparator<WebElement>
         double r1 = Double.parseDouble(o1.getAttribute("r"));
         double r2 = Double.parseDouble(o2.getAttribute("r"));
         if (r1 < r2)
-            return -1;
+            return 1;
         else if (r1 == r2)
             return 0;
         else
-            return 1;
+            return -1;
     }
 }
--- test/d3/demo/VaxDemo.java
+++ test/d3/demo/VaxDemo.java
@@ -11,8 +11,10 @@ import org.openqa.selenium.remote.D3Element;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import other.SupportedDriver;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import static d3.demo.DemoConstants.*;
@@ -33,12 +35,14 @@ public class VaxDemo
     {
         startGame();
         vaccinationPhase();
+        quarantinePhase();
+        System.out.println("Game Complete");
     }
 
     private void startGame()
     {
         driver = SupportedDriver.Chrome.getDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(VAX_URL);
         WebElement button = driver.findElement(By.cssSelector(EASY_DIFFICULTY));
         button.click();
@@ -48,6 +52,7 @@ public class VaxDemo
 
     private void vaccinationPhase() throws Exception
     {
+        System.out.println("-==Entering Vaccination Phase==-");
         int vaccinesLeft = getRemainingVaccines();
         while (vaccinesLeft > 0)
         {
@@ -60,13 +65,88 @@ public class VaxDemo
         }
     }
 
+    private void quarantinePhase() throws Exception
+    {
+        System.out.println("-==Entering Quarantine Phase==-");
+        Thread.sleep(7000);
+        while (!gameOver())
+        {
+            //Wait for infection to spread
+            Thread.sleep(3000);
+
+            //find infected nodes
+            List<D3Element> infected = getInfectedNodes();
+            System.out.println("Number Infected: " + Integer.toString(infected.size()));
+
+            //Get list of potential targets
+            List<D3Element> neighbors = getNeighborsOfInfected(infected);
+            if (neighbors.size() == 0)
+                System.out.println("Cannot find anyone to quarantine");
+            else
+            {
+               D3Element quarantinee = getNextQuarantineTarget(neighbors);
+                System.out.println("clicking on: " + quarantinee.getId());
+               quarantinee.click();
+            }
+        }
+        System.out.println("End Quarantine Phase");
+    }
+
+    private D3Element getNextQuarantineTarget(List<D3Element> neighbors)
+    {
+        Collections.sort(neighbors,new NodeSizeComparator());
+        for (int i = 0; i < neighbors.size(); i++)
+        {
+            if (!isInfected(neighbors.get(i)))
+                return neighbors.get(i);
+        }
+        throw new NoSuchElementException("There are no 'neighbors' that aren't infected!");
+    }
+
+    private List<D3Element> getInfectedNodes()
+    {
+        List<WebElement> infected = driver.findElements(ByD3.svg().shape("circle").withAttributes(INFECTED_ATTRIBUTES));
+        List<D3Element> result = new ArrayList<>();
+        for (WebElement node : infected)
+        {
+            result.add((D3Element)node);
+        }
+        return result;
+    }
+
+    private List<D3Element> getNeighborsOfInfected(List<D3Element> infected)
+    {
+        List<D3Element> neighbors = ForceLayout.getAllNeighbors(infected);
+        return neighbors;
+    }
+
+    private boolean isInfected(D3Element node)
+    {
+        String style = node.getAttribute("style");
+        if (style.equals(INFECTED))
+            return true;
+        return false;
+    }
+
     private D3Element getNextVaccinationTarget()
     {
-        List<WebElement> nodes = driver.findElements(ByD3.svg().shape("circle").withAttribute("class","node"));
+        List<WebElement> nodes = driver.findElements(ByD3.svg().shape("circle").withAttributes(NODE_ATTRIBUTES));
         Collections.sort(nodes,new NodeSizeComparator());
 
         WebElement largest = nodes.get(0);
         return new D3Element(largest);
+    }
+
+    private boolean gameOver()
+    {
+        try
+        {
+            WebElement submit = driver.findElement(By.cssSelector(SUBMIT_TEXT));
+            return submit != null;
+        }
+        catch (Exception ignore)
+        {}
+        return false;
     }
 
     private int getRemainingVaccines() throws Exception
