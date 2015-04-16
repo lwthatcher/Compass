23274558f69f783e9f4cd352a84305ce3e05478c
Lawrence Thatcher <lwthatcher@msn.com>
2015-04-16 10:26:55 -0600

    additional locators in force layout

--- src/d3/element/ForceLayout.java
+++ src/d3/element/ForceLayout.java
@@ -1,5 +1,8 @@
 package d3.element;
 
+import d3.by.shape.ByShape;
+import d3.by.shape.D3Shape;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.D3Element;
 
@@ -20,6 +23,25 @@ public class ForceLayout extends D3Element
         super(e);
     }
 
+    //TODO: extract out into abstract class for layouts or svg elements
+    public By byShape(String... shapes)
+    {
+        D3Shape[] args = new D3Shape[shapes.length];
+        int i = 0;
+        for (String shape : shapes)
+        {
+            args[i] = D3Shape.toShape(shape);
+            i++;
+        }
+        return new ByShape(null,args);
+    }
+
+    public By byShape(D3Shape... shapes)
+    {
+        return new ByShape(null,shapes);
+    }
+
+    //Static Methods
     public static List<D3Element> getAllNeighbors(List<D3Element> nodes)
     {
         Set<D3Element> totalNeighbors = new HashSet<D3Element>();
---
+++ src/d3/element/ForceNode.java
@@ -0,0 +1,17 @@
+package d3.element;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.D3Element;
+
+/**
+ * Class that represents a node in the D3 force layout.
+ *
+ * @author Lawrence Thatcher
+ */
+public class ForceNode extends D3Element
+{
+    public ForceNode(WebElement e)
+    {
+        super(e);
+    }
+}
